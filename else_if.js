40c2d6e271d12299e340ac9b04469b2cc9f505d9
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-08-16 22:41:32 +0530

    if else practice

---
+++ else_if.js
@@ -0,0 +1,20 @@
+const weekDay = "Monday";
+
+if (weekDay == "Monday") {
+  /// While giving the string we need to give it in a string
+  console.log("Complete an exam");
+} else if (weekDay == "Tuesday") {
+  console.log("Set a meeting with client");
+} else if (weekDay == "Wednesday") {
+  console.log("Prepare Javascript");
+} else if (weekDay == "thursday") {
+  console.log("parctice javascript");
+} else if (weekDay == "friday") {
+  console.log("prepare DOM manupulation");
+} else if (weekDay == "saturday") {
+  console.log("practice DOM");
+} else if (weekDay == "sunday") {
+  console.log("Have a funday");
+} else {
+  console.log("Please enter the valid day");
+}
---
+++ script.js
@@ -0,0 +1,16 @@
+// Syntax :
+/*if(condtion){
+  statement1;
+}else{
+  statement2;
+}*/
+
+var a = 234;
+var b = 567;
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("b is greater than a");
+}
+
+a > b ? console.log("a is greater than b") : console.log("b is greater than a");
